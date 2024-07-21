3444f7e5e88bf310c471395b9ec7ac000fdfbaa7
Liene Briede <lienebriede84@gmail.com>
2024-07-21 13:08:33 +0000

    Add Content

---
+++ my-app/src/components/Content.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import css from './css/Content.module.css';
+import { savedPosts } from '../posts.json'
+import PostItem from './PostItem';
+
+
+
+export class Content extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+        }
+    }
+    render() {
+        return (
+            <div className={css.Content}>
+                <div className={css.TtitleBar}>
+                    <h1>My Posts</h1>
+                </div>
+                <div className={css.SearchResults}>
+                    <PostItem savedPosts={savedPosts} />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Content
