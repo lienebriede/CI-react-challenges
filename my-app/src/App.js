d911dc3aa4d2b192f17b3509d166683d71853baa
Liene Briede <lienebriede84@gmail.com>
2024-07-21 10:04:10 +0000

    Nav Bar Form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import css from './App.module.css';
-import NavBarSimple from './components/NavBarSimple';
+//import NavBarSimple from './components/NavBarSimple';
+import Sidebar from "./components/Sidebar";
+import NavBarForm from './components/NavBarForm';
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarSimple />
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
     </div>
   );
 }
