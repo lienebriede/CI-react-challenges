3704865930dad97d9b5648a75be73aeed6579657
Liene Briede <lienebriede84@gmail.com>
2024-07-28 11:25:09 +0000

    Add search function to saved posts

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,28 +10,50 @@ export class Content extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: []
         }
     }
 
     componentDidMount() {
         setTimeout(() => {
             this.setState({
-                isLoaded: true
+                isLoaded: true,
+                posts: savedPosts
             })
         }, 2000)
 
+    }
+
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter(post => {
+            return post.name.toLowerCase().includes(name);
+        })
+        this.setState({
+            posts: filteredPosts
+        })
     }
     render() {
         return (
             <div className={css.Content}>
                 <div className={css.TtitleBar}>
                     <h1>My Posts</h1>
+                    <form>
+                        <label htmlFor='searchInput'>Search:</label>
+                        <input 
+                        onChange={(e) => this.handleChange(e)}
+                        type='search' 
+                        id='searchInput' 
+                        /> 
+                    </form>
+                    <h4>posts found: {this.state.posts.length}</h4>
+                    
                 </div>
                 <div className={css.SearchResults}>
                     {
                         this.state.isLoaded ?
-                            <PostItem savedPosts={savedPosts} />
+                            <PostItem savedPosts={this.state.posts} />
                             : <Loader />
                     }
                 </div>
