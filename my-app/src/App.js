c43e5afc3d32befcc364e84c94ef6e6a16166886
Liene Briede <lienebriede84@gmail.com>
2024-07-29 10:12:07 +0000

    Change content to contentHooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import css from './App.module.css';
 //import NavBarSimple from './components/NavBarSimple';
 import Sidebar from "./components/Sidebar";
 import NavBarForm from './components/NavBarForm';
-import Content from './components/Content';
+import ContentHooks from './components/ContentHooks';
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       {/* Add your components here */}
       <NavBarForm />
       <Sidebar />
-      <Content />
+      <ContentHooks />
     </div>
   );
 }
